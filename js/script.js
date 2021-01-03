18a6767acd20d9a976ee983313bfa91283d45999
creasedigital <ojorechris@gmail.com>
2021-01-03 07:00:14 +0100

    API config added

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$('document').ready(function(){
+    const url = 'http://hn.algolia.com/api/v1/search?query=javascript'
+
+    $.getJSON(url, (data) => {
+
+let currentQuote = ''
+const quotes = data.hits
+
+if (currentQuote === '') {
+    $('quoteUrl').css('display', 'none')   
+} else{
+
+}
+
+$('.read').on('click', () => {
+    currentQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    console.log(currentQuote)
+
+    $('.quoteAuthor').html(currentQuote.author)
+    $('.quoteBody').html(currentQuote.title)
+    $('.tweetQuote').attr({href: 'https://twitter.com/intent/tweet?text= ' + currentQuote.title + ' - ' + currentQuote , 
+        target: '_blank'
+        })
+
+    $('.tweetQuote').props('disabled', false)
+
+    $('.quoteUrl').css('display', 'block')
+
+    $('.quoteUrl').on('click', () =>{
+
+        $('.quoteUrl').attr({href: currentQuote.url, 
+    target: '_blank'
+    })
+    })
+})
+
+
+
+
+})
+
+
+})
